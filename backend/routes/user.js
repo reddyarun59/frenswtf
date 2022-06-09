572900f180a9311ffb0e31fb6083d6eb1c0f78d6
reddyarun59 <reddyarun59@yahoo.com>
2022-06-09 10:14:29 +0530

    Added Auth Middleware and API for search All Users

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { registerUser, authUser } = require('../controllers/user');
+const { registerUser, authUser, allUsers } = require('../controllers/user');
+const { protect } = require('../middleware/authMiddleware');
 
 const router=express.Router()
 
-router.route("/").post(registerUser)
+router.route("/").post(registerUser).get(protect, allUsers)
 
 router.route("/login").post(authUser)
 
